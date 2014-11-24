6ae4831bfea709c826abeb98de491e3a69da687c
Ivy <wongivy13@yahoo.com>
2014-11-23 20:00:43 -0800

    Added map and marker. InfoWindow shows the street address and camera url.

--- js/app.js
+++ js/app.js
@@ -11,3 +11,50 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function() {
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    var stations;
+    var markers = [];
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function(data, itemIndex) {
+            stations = data;
+            data.forEach(function(station) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(station.location.latitude),
+                        lng: Number(station.location.longitude)
+                    },
+                    map: map
+                });
+                markers.push(marker);
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    map.panTo(marker.getPosition());
+
+                    var html = '<p>' + station.cameralabel + '<p>';
+                    html += '<img src="' + station.imageurl.url + '" alt="Traffic Camera Picture">';
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+            });
+        })
+        .fail(function(error) {
+            console.log(error);
+        })
+        .always(function() {
+            $('#ajax-loader').fadeOut();
+        });
+});
